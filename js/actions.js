22000ea6e60944cb03f979302fac6136f77269fd
vamshi reddy <124848383+vamshi1100@users.noreply.github.com>
2024-11-22 18:57:12 +0530

    new changes mvc
    changes made

---
+++ js/actions.js
@@ -0,0 +1,22 @@
+class actions {
+  constructor() {}
+
+  //calculate
+
+  calculate() {}
+  //add history
+
+  history() {}
+
+  //update history
+  update() {}
+  //delete history
+
+  delete() {}
+
+  //display history
+  display() {}
+
+  //filter
+  filter() {}
+}
